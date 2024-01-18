aab53d12b922e9ed192ce7aa1e445205461fc223
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-01-18 20:46:36 +0530

    added

---
+++ dice/src/App.js
@@ -0,0 +1,17 @@
+import { Flex, Heading, Stack } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+    <Flex>
+      <Image src = "dices.png"/>
+      <Stack>
+        <Heading as = "h1">The Dice Game</Heading>
+      </Stack>
+    </Flex>
+    </div>
+  );
+}
+
+export default App;
